5066233f524cef162cd49e529a6c827e16ffa12e
Mefuuuu <133778142+Mefuuuu@users.noreply.github.com>
2023-06-23 20:10:39 +0700

    Add files via upload

---
+++ Tháng 1: C/B11 BaitapOOP/b1.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+class Diem{
+    private: 
+        int8_t toaDoX;
+        int8_t toaDoY;
+    public: 
+        Diem(int8_t x, int8_t y);
+        Diem();
+        void setToaDoX(int x);
+        int8_t getToaDoX();
+};
+
+Diem::Diem(int8_t x , int8_t y){
+    toaDoX = x; 
+    toaDoY = y;
+}
+class PhepToanOXY{
+    private: 
+        Diem diemA;
+        Diem diemB;
+        Diem diemC;
+        typedef enum{
+            KHOANG_CACH,
+            DIEN_TICH_TAM_GIAC
+        }TypePhepToan; 
+
+        TypePhepToan phepToan;
+    public: 
+            PhepToanOXY(Diem A, Diem B);
+            PhepToanOXY(Diem A, Diem B, Diem C);
+            float getKhoangCach();
+            float getDienTich();
+};
+
+PhepToanOXY::PhepToanOXY(Diem A, Diem B){
+    phepToan = KHOANG_CACH;
+    diemA = A; 
+    diemB = B; 
+}
+
+PhepToanOXY::PhepToanOXY(Diem A, Diem B, Diem C){
+    phepToan = DIEN_TICH_TAM_GIAC; 
+    diemA = A;
+    diemB = B;
+    diemC = C;
+}
+
+float PhepToanOXY::getKhoangCach(){
+    if(phepToan == KHOANG_CACH){
+        return 0;
+    }else {
+        printf("Khong goi duoc ham tinh khoang cach");
+    }
+}
+
+int main()
+{
+    /* code */
+    return 0;
+}
